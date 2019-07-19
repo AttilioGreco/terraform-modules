9da2bac7654fb03257d2966e786c707ef4fc2503
Attilio Greco <attilio.greco.gapmilano@gmail.com>
2019-07-19 20:23:03 +0200

    add

--- openstack/network/main.tf
+++ openstack/network/main.tf
@@ -35,7 +35,7 @@ resource "openstack_networking_router_interface_v2" "int-ext-interface-existing"
 // if a router does nit exist exist do this instead
 resource "openstack_networking_router_v2" "router" {
   count = "${var.router_id != "" ? 0 : 1}"
-  name = "router"
+  name = "${var.router_name}"
   region = "${var.region}"
   admin_state_up = "true"
   external_network_id =  "${data.openstack_networking_network_v2.public_network.id}"
